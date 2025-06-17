804b4a32f387641b6fe0c131ba7b9e438b41b756
Magnus-Mage <romescha123@gmail.com>
2025-06-17 06:42:25 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,6 +39,18 @@ resource "openstack_compute_instance_v2" "control_plane" {
   }
 }
 
+data "openstack_networking_network_v2" "external" {
+  name = "External_Net"  # or whatever your external network is named (often "external" or "public")
+}
+
+resource "openstack_networking_router_v2" "router" {
+  name                = "router-k8s"
+  admin_state_up      = true
+  external_network_id = data.openstack_networking_network_v2.external.id
+}
+
+
+
 resource "openstack_networking_port_v2" "control_plane" {
   count          = 3
   name           = "port-k8s-master-${count.index + 1}"
@@ -108,4 +120,61 @@ resource "openstack_compute_secgroup_v2" "ssh" {
     cidr        = "0.0.0.0/0"
   }
 }
+
+
+
+resource "openstack_networking_router_interface_v2" "router_interface" {
+  router_id = openstack_networking_router_v2.router.id
+  subnet_id = openstack_networking_subnet_v2.http.id
+}
+
+
+resource "openstack_compute_instance_v2" "proxy_vm" {
+  name        = "proxy-vm"
+  image_name  = var.image
+  flavor_name = var.flavor_http
+  key_pair    = openstack_compute_keypair_v2.user_key.name
+  user_data   = file("../scripts/setup-proxy.sh")
+
+  network {
+    port = openstack_networking_port_v2.proxy_internal.id
+  }
+
+  network {
+    port = openstack_networking_port_v2.proxy_external.id
+  }
+
+  block_device {
+    volume_size           = var.volume_http
+    destination_type      = "volume"
+    delete_on_termination = true
+    source_type           = "image"
+    boot_index            = 0
+    uuid                  = "cfe32483-65b9-4532-9459-ee6904c02e09"
+  }
+}
+
+
+resource "openstack_networking_port_v2" "proxy_internal" {
+  name           = "proxy-internal-port"
+  network_id     = openstack_networking_network_v2.generic.id
+  admin_state_up = true
+  fixed_ip {
+    subnet_id = openstack_networking_subnet_v2.http.id
+  }
+  security_group_ids = [
+    openstack_compute_secgroup_v2.ssh.id,
+  ]
+}
+
+resource "openstack_networking_port_v2" "proxy_external" {
+  name           = "proxy-external-port"
+  network_id     = data.openstack_networking_network_v2.external.id
+  admin_state_up = true
+  security_group_ids = [
+    openstack_compute_secgroup_v2.ssh.id,
+  ]
+}
+
+
 
